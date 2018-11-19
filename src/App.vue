4686e7d5a25d9e2ee7487ebd97218481404d8f7d
ktr0328 <zf7heatpa@gmail.com>
2018-11-19 19:56:18 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click='toolbar = !toolbar' />
+      <v-toolbar-title class="headline">
+        for-vue-handson
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-navigation-drawer
+      app
+      v-model='toolbar'
+    >
+      <v-list>
+        <v-list-tile to='/'>
+          <v-list-tile-avatar>
+            <v-icon color="teal">fas fa-home</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to='/about'>
+          <v-list-tile-avatar>
+            <v-icon color="teal">fas fa-shapes</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>about</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      toolbar: true
     }
   }
 }
-</style>
+</script>
